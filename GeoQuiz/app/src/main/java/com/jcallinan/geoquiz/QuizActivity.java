6455c7c40b7f9eccb1e4312ba70a3bee4158ccc3
Jeremy Callinan <jeremy.callinan@gmail.com>
2024-01-18 20:27:05 -0500

    First version
    Around page 70 in Big Nerd Ranch Guide V2

---
+++ GeoQuiz/app/src/main/java/com/jcallinan/geoquiz/QuizActivity.java
@@ -0,0 +1,47 @@
+package com.jcallinan.geoquiz;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.core.view.WindowCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.jcallinan.geoquiz.databinding.ActivityQuizBinding;
+
+public class QuizActivity extends AppCompatActivity {
+
+   private Button mTrueButton;
+   private Button mFalseButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+        mTrueButton = findViewById(R.id.true_button);
+        mFalseButton = findViewById(R.id.false_button);
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(QuizActivity.this,R.string.correct_toast,Toast.LENGTH_LONG).show();
+            }
+        });
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(QuizActivity.this,R.string.incorrect_toast,Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+
+}
